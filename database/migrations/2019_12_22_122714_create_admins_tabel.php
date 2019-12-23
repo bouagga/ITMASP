343237d6b3697b338ec7bbc3aad38a093df9e635
islambouagga <44034848+islambouagga@users.noreply.github.com>
2019-12-23 19:24:43 +0100

    test commit

--- database/migrations/2019_12_22_122714_create_admins_tabel.php
+++ database/migrations/2019_12_22_122714_create_admins_tabel.php
@@ -14,7 +14,7 @@ class CreateAdminsTabel extends Migration
     public function up()
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->bigIncrements('id_admin');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
